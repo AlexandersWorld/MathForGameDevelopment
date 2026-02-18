a8e5a3e7f1f01f5e69446d17217c38c3d7c677dc
Ale Pereira <naitwa-alexandre@gmail.com>
2026-02-18 13:51:39 -0300

    mouse rotation on axis pitch, yaw and roll

---
+++ CharacterMoviment/CharacterMoviment/EAngle.cpp
@@ -0,0 +1,32 @@
+#include "Vector2.h"
+
+#include <cmath>
+#include "EAngle.h"
+
+Vector EAngle::ToVector() const
+{
+	Vector result;
+
+	result.x = cos(y) * cos(p);
+	result.y = sin(p);
+	result.z = sin(y) * cos(p);
+
+	return result;
+}
+
+void EAngle::Normalize()
+{
+	if (p > 89)
+		p = 89;
+	if (p < -89)
+		p = -89;
+
+	while (y < -180)
+	{
+		y += 360;
+	}
+	while (y > 180)
+	{
+		y -= 360;
+	}
+}
